e36eca622ade8993834f5a10869e87dfd529dcd4
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-10-15 10:52:28 +0300

    fixed bags 2.0

--- src/tasks/task1.js
+++ src/tasks/task1.js
@@ -3,6 +3,7 @@
 */
 const sayHello = (name) => {
     if (typeof name === "string") {
+        name = name.charAt(0).toUpperCase() + name.slice(1);
         if (name === "Mark") return `Hi, ${name}!`;
         return `Hello, ${name}!`;
     }
@@ -11,16 +12,17 @@ const sayHello = (name) => {
 
 console.log(sayHello("Oleg"));
 console.log(sayHello("Viktor"));
-console.log(sayHello("Mark"));
+console.log(sayHello("mark"));
 
 function sayHelloVers2(name){
     if (typeof name === "string") {
+        name = name.charAt(0).toUpperCase() + name.slice(1);
         if (name === "Mark") return `Hi, ${name}!`;
         return `Hello, ${name}!`;
     }
-    return "Параметр не строка";     
+    return "Параметр не строка";       
 }
 
 console.log(sayHelloVers2("Oleg"));
 console.log(sayHelloVers2("Viktor"));
-console.log(sayHelloVers2("Mark"));
+console.log(sayHelloVers2("mark"));
--- src/tasks/task2.js
+++ src/tasks/task2.js
@@ -2,8 +2,12 @@
     Функкция находит минимальное из двух чисел 
 */
 
+
 const findMinNumber = (firstNumber, secondNumber) => {
-    if (typeof firstNumber === "number" && typeof secondNumber === "number") return (firstNumber < secondNumber) ? firstNumber : secondNumber;
+    if (typeof firstNumber === "number" && typeof secondNumber === "number") {
+        if (firstNumber === secondNumber) return "Числа равны";
+        return Math.min(firstNumber, secondNumber);
+    }
     return "Исходные данные должны быть числами";
 }
 
@@ -13,7 +17,10 @@ console.log(findMinNumber(-16, 28));
 console.log(findMinNumber("abc", 28));
 
 function findMinNumberVers2(firstNumber, secondNumber) {
-    if (typeof firstNumber === "number" && typeof secondNumber === "number") return (firstNumber < secondNumber) ? firstNumber : secondNumber;
+    if (typeof firstNumber === "number" && typeof secondNumber === "number") {
+        if (firstNumber === secondNumber) return "Числа равны";
+        return Math.min(firstNumber, secondNumber);
+    }
     return "Исходные данные должны быть числами";
 }
 
