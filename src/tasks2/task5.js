4f164e9b3ca00bf39292b030ffbfae7a6390e8ea
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-10-22 19:54:02 +0300

    added fifth task

---
+++ src/tasks2/task5.js
@@ -0,0 +1,21 @@
+// TODO: 5. Написать функцию, которая принимает массив, состоящий их разных типов данных. 
+// Фукнция возвращает массив, в котором все числа принятого массива умножаются на 2 и становятся строками,
+// все символы в строковых элементах исходного массива становятся
+// в верхнем регистре, все элементы, которые не являются ни строкой, ни числом отсеиваются.
+
+
+const modifyArray = (mixedArray) => {
+    if (Array.isArray(mixedArray)) {      
+        mixedArray = mixedArray.filter(element => typeof element === 'number' || typeof element === 'string');
+        return mixedArray.map(element => {
+            if (typeof element === "number") return 2 * element;
+            else if (typeof element === "string") return element.toUpperCase();
+        });
+    }
+    return "Error";
+}
+
+
+exampleArray = [2, 4, null, undefined, "hello", "World", -8, 0];
+
+console.log(modifyArray(exampleArray));
