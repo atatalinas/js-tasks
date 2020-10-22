6bf8e12e06436094d386cf59451c8dccbeb6f8ef
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-10-22 12:03:26 +0300

    added first task

--- src/tasks2/task1.js
+++ src/tasks2/task1.js
@@ -1,1 +1,19 @@
-console.log("hello world!");
+// TODO: 1. Написать функцию, которая принимает массив чисел. Функция должна вернуть массив, в котором все элементы
+// принятого массива умножены на 3;
+
+
+const multiplyArrayElements = (numericArray) => {
+    if (Array.isArray(numericArray)) {
+        if (numericArray.every(elem => typeof elem === "number")) {
+            numericArray.forEach(((element, index) => numericArray[index] = 3 * element));
+            return numericArray;
+        } 
+    }
+    return "Error";
+}
+
+numberArray = [10, 20, 30, 40, 50, 60, 70];
+numberArray2 = [-3, 0, 'sad'];
+
+console.log(multiplyArrayElements(numberArray));
+console.log(multiplyArrayElements(numberArray2));
