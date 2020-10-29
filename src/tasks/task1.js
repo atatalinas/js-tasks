a3a5ff46787f698fc4382705305170ac4474ae9c
Ангелина Статкевич <angelina.statkevich@gmail.com>
2020-10-29 23:40:43 +0300

    implemented task1

--- src/tasks/task1.js
+++ src/tasks/task1.js
@@ -4,44 +4,46 @@
 (first_name и last_name). Количество пользователей может быть не ограничено. */
 
 const users = [
-    {
-        id: 1,
-        first_name: 'Jeanette',
-        last_name: 'Penddreth',
-        email: 'jpenddreth0@census.gov',
-        gender: 'Female',
-        ip_address: '26.58.193.2',
-    },
-    {
-        id: 2,
-        first_name: 'Petr',
-        last_name: 'Jackson',
-        email: 'gfrediani1@senate.gov',
-        gender: 'Male',
-        ip_address: '229.179.4.212',
-    },
+	{
+		id: 1,
+		first_name: 'Jeanette',
+		last_name: 'Penddreth',
+		email: 'jpenddreth0@census.gov',
+		gender: 'Female',
+		ip_address: '26.58.193.2',
+	},
+	{
+		id: 2,
+		first_name: 'Petr',
+		last_name: 'Jackson',
+		email: 'gfrediani1@senate.gov',
+		gender: 'Male',
+		ip_address: '229.179.4.212',
+	},
 ];
 
 
-/* Пример результата выполнения:
-{
-women: [
-  {
-    id: 1,
-    fullName: 'Jeanette Penddreth',
-    email: 'jpenddreth0@census.gov',
-    gender: 'Female',
-    ipAddress: '26.58.193.2',
-  }
-],
-men: [
-  {
-    id: 2,
-    fullName: 'Petr Jackson',
-    email: 'gfrediani1@senate.gov',
-    gender: 'Male',
-    ip_address: '229.179.4.212',
-  }
-]
+const sortByGender = (usersArray) => {
+	if (!Array.isArray(usersArray)) return;
+	const result = {};
+	result['women'] = [];
+	result['men'] = [];
+	usersArray.forEach((user, index) => {
+		const copyUser = { ...user };
+		if (copyUser.hasOwnProperty('first_name') && copyUser.hasOwnProperty('last_name')) {
+			copyUser['fullName'] = `${copyUser.first_name} ${copyUser.last_name}`;
+			delete copyUser.first_name;
+			delete copyUser.last_name;
+		}
+		if (copyUser.hasOwnProperty('gender') && copyUser.gender === "Female") {
+			result['women'].push(copyUser);
+		} else {
+			result['men'].push(copyUser);
+		}
+	})
+
+	return result;
 }
-*/
+
+
+console.log(sortByGender(users));
